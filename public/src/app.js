fbb0c848680ea79cd4deef99d84317dea6223d60
Pawnyy <sfaronoff@gmail.com>
2022-02-23 12:20:36 -0500

    Update app.js

--- public/src/app.js
+++ public/src/app.js
@@ -11,22 +11,24 @@ const transcode = async ({ target: { files } }) => {
     for (let i = 1; i<=files.length; i++) {
         let file = files[i-1]
         const name = file.name;
+        let dropdown = document.getElementById('formatDd');
+        const format = dropdown.value;
 
         let elem = document.createElement('a')
-        elem.innerText = `(${i}/${files.length}) Loading... ${name.replace(name.substr(name.lastIndexOf('.')), '')}.mp3`
+        elem.innerText = `(${i}/${files.length}) Loading... ${name.replace(name.substr(name.lastIndexOf('.')), '')}.${format}`
         document.body.appendChild(elem)
         document.body.appendChild(document.createElement('br'))
 
         const arrayBuffer = await file.arrayBuffer()
         const uint8Array = new Uint8Array(arrayBuffer);
         ffmpeg.FS("writeFile", name, uint8Array);
-        await ffmpeg.run('-i', name, 'test.mp3');
-        const data = ffmpeg.FS('readFile', 'test.mp3');
+        await ffmpeg.run('-i', name, 'test.'+format);
+        const data = ffmpeg.FS('readFile', 'test.'+format);
         // const track = document.getElementById("track");
         // track.src = URL.createObjectURL(new Blob([data.buffer], { type: 'audio/mpeg' }));
         elem.href = URL.createObjectURL(new Blob([data.buffer], { type: 'audio/mpeg' }))
-        elem.download = name.replace(name.substr(name.lastIndexOf('.')), '') + '.mp3'
-        elem.innerText = name.replace(name.substr(name.lastIndexOf('.')), '') + '.mp3'
+        elem.download = name.replace(name.substr(name.lastIndexOf('.')), '') + '.'+format
+        elem.innerText = name.replace(name.substr(name.lastIndexOf('.')), '') + '.'+format
     }
 
 }
